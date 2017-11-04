0e9164f4b924509acf924a057b7c70594e36eeb7
Brendan Kellams <brendan.kellams@gmail.com>
2017-11-04 04:12:42 -0700

    nearly finished code

--- keys.js
+++ keys.js
@@ -1,11 +1,11 @@
-console.log('this is loaded');
+
 
 var twitterKeys = {
-  consumer_key: 'xIhLlsixNjhi16K25MJHADU1N',
-  consumer_secret: 'KHqvdcjEIppwbo3OyG6pURkuR3ZIDwaJTm7dvGYPWXs22fNKXE',
+  consumer_key: 'mR6r0InNSLcleQiHkEOQXVAVe',
+  consumer_secret: 'QZ1J94VG3tH6LH8f12hPCJ4OUqvI6ZMd8yB97DvPiOO603JQiY',
   access_token_key: '844740670191370240-49qufoS46XzRQdTdoBPPPIxuQOil9hD',
-  access_token_secret: 'pLsHLVqCM8SN6SriTbkiO79AbhPPJtXOQmPeq4DDxf3VG=',
-}
+  access_token_secret: 'pLsHLVqCM8SN6SriTbkiO79AbhPPJtXOQmPeq4DDxf3VG',
+};
 
 var spotify = {
   id: 'f2b217cd33584c6e9a86dafda50d3e3a',
--- liri.js
+++ liri.js
@@ -1,97 +1,122 @@
-// var fs = require('fs');
-var keys = require("./keys.js");
-// var request = require('request');
-var Spotify = require('node-spotify-api');
-// var twitter = require('twitter');
-var spClient = new Spotify(keys.spotify);
-// var twClient = new Twitter(keys.twitterKeys);
-
-
-
-
+// node arguments
 var command = process.argv[2];
 var value = process.argv[3];
 
-// if (command === "movie-this") {
-//     movie();
-// }
-// else if (command === "my-tweets") {
-//     tweet();
-// }
- if (command === "spotify-this-song") {
-    song();
+// my require modules
+var request = require('request');
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var fs = require('fs');
+
+// keys
+var keys = require("./keys.js");
+var spClient = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitterKeys);
+
+// username for twitter
+var params = {
+    screen_name: 'brendan_kellams',
+    count: 20
 }
-// else if (command === "do-what-it-says") {
-//     grab();
-// }
-// else {
-//     console.log("I'm sorry, please enter a valid option:\n" + "1. movie-this \n" +
-//         "2. my-tweets\n" + "3. spotify-this-song\n" + "4. do-what-it-says\n")
-// }
+
+commands();
+
+// User input for process.argv[3]
+function commands() {
+    if (command === "movie-this") {
+        movie();
+    }
+    else if (command === "my-tweets") {
+        tweet();
+    }
+    else if (command === "spotify-this-song") {
+        song();
+    }
+    else if (command === "do-what-it-says") {
+        doSomething();
+    }
+    else {
+        console.log("I'm sorry, please enter a valid option:\n" + "1. movie-this \n" +
+            "2. my-tweets\n" + "3. spotify-this-song\n" + "4. do-what-it-says\n")
+    }
+
+}
 
 // Request with OMDB
-// function movie() {
-//     var movieNameToken = process.argv.slice(3);
-//     var movieName = movieNameToken.join('+');
-//     request('http://www.omdbapi.com/?apikey=40e9cece&t=' + movieName + "&y=&plot=short&tomatoes=true&r=json", function (error, response, body) {
-//         var data = JSON.parse(body);
-//         var rating = data["Ratings"];
-//         function checkRating(rating) {
-//             return rating.Source === "Rotten Tomatoes";
-//         }
-//         if (!error && response.statusCode == 200) {
-//             console.log("Title: " + data.Title);
-//             console.log("Year: " + data.Year);
-//             console.log("Imdb Rating: " + data.imdbRating);
-//             console.log("Rotten Tomatoes Rating: " + data.Ratings.find(checkRating).Value);
-//             console.log("Country: " + data.Country);
-//             console.log("Language: " + data.Language);
-//             console.log("Plot: " + data.Plot);
-//             console.log("Actors: " + data.Actors);
-//         } else {
-//             console.log(error);
-//         }
-//     });
-// }
+function movie() {
+    var movieNameToken = process.argv.slice(3);
+    var movieName = movieNameToken.join('+');
+    request('http://www.omdbapi.com/?apikey=40e9cece&t=' + movieName + "&y=&plot=short&tomatoes=true&r=json", function (error, response, body) {
+        var data = JSON.parse(body);
+        var rating = data["Ratings"];
+        function checkRating(rating) {
+            return rating.Source === "Rotten Tomatoes";
+        }
+        if (!error && response.statusCode == 200) {
+            console.log("Title: " + data.Title);
+            console.log("Year: " + data.Year);
+            console.log("Imdb Rating: " + data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + data.Ratings.find(checkRating).Value);
+            console.log("Country: " + data.Country);
+            console.log("Language: " + data.Language);
+            console.log("Plot: " + data.Plot);
+            console.log("Actors: " + data.Actors);
+        } else {
+            console.log(error);
+        }
+    });
+}
 
 // Spotify
-function song() {
+function song(song) {
     var song = process.argv.slice(3);
-    var songJoin = song.join(" ");
-    spClient.search({ type: 'track', query: songJoin, limit: 1 }, function (error, data, body) {
-        if (!error && data.statusCode == 200) {
-            jsonBody = JSON.parse(data);
-            console.log('success', data);
-            console.log(body);
+    var songJoin = song.join(" ").trim();
 
-        } else {
+    spClient.search({ type: 'track', query: songJoin, limit: 1 }, function (error, data) {
+        if (!error) {
+            var albums = data.tracks.items[0];
+            console.log("Artist: " + albums.artists[0].name);
+            console.log("Song: " + albums.name);
+            console.log("Album: " + albums.album.name);
+            console.log("Preview URL: " + albums.preview_url);
+        }
+        else {
             console.log('error', error);
         }
     });
-        
-} 
-// end spotifyThis function
+}
+
+// Twitter
+function tweet() {
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error && response.statusCode == 200) {
+            console.log(' ');
+            console.log('Last 20 Tweets:')
+            for (i = 0; i < tweets.length; i++) {
+                var number = i + 1;
+                console.log(' ');
+                console.log([i + 1] + '. ' + tweets[i].text);
+                console.log('Created on: ' + tweets[i].created_at);
+                console.log(' ');
+            }
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+// Grabbing function
+function doSomething() {
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if (!error) {
+            results = data.split(",");
+            song(results[1]);
+        } else {
+            console.log("Error occurred" + error);
+        }
+    });
+};
 
 
 
-
-
-
-
-
-// var twitter = require('twitter');
-// var T = new twitter(twitterKeys);
-
-// var commands = process.argv[2];
-
-// var params = { screen_name: "brendan_kellams", count: 20 };
-// T.get('statuses/user_timeline', params, function (error, tweets, response) {
-//     if (!error) {
-//         console.log(tweets);
-//     } else {
-
-//     }
-// });
-// var request = require('request');
-// var spotify = require('node-spotify-api');
 
